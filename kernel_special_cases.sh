6427cd5450ff33cc063f2ef6a4f0ac9911d3408b
nuclearmistake <nuclearmistake@gmail.com>
2015-04-30 07:17:51 -0400

    Freeze M7 kernel at its L5 ref before it was unceremoniously deleted
    Change-Id: I6749a04b37df06ff58f22a1996221da291aec4cc
    
    Conflicts:
    	kernel_special_cases.sh

--- kernel_special_cases.sh
+++ kernel_special_cases.sh
@@ -34,9 +34,9 @@ case $1 in
         export kernelsource="kernel_htc_dlx"
     ;;
     m7*)
-        export remote="github"
-        export remoterevision="lollipop"
-        export kernelsource="cannondalev2000/kernel_htc_m7"
+        export remote="vanir"
+        export remoterevision="L5"
+        export kernelsource="kernel_htc_m7"
     ;;
     find5|n1)
         export remote="vanir"
