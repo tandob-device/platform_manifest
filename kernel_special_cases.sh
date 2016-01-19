9d621e25fa461b7eccca4c9d969ad672f8ff7331
DHOMD <DHO@dho.im>
2016-01-19 17:11:16 -0500

    Cancro needs speacil cases
    Change-Id: I00aa40514e1b6312bbfafe9780fecfbea83afa7f

--- kernel_special_cases.sh
+++ kernel_special_cases.sh
@@ -24,6 +24,11 @@ case $1 in
         export remoterevision="mashmellow"
         export kernelsource="kernel-hut/android_kernel_xiaomi_cancro"
     ;;
+    cancro)
+        export remote="github"
+        export remoterevision="mashmellow"
+        export kernelsource="kernel-hut/android_kernel_xiaomi_cancro"
+    ;;
     d800|d801|d802|d803|f320|ls980|vs980)
         export remote="vanir"
         export remoterevision="m6"
